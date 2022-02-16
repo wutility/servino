e206cbe40a33a83d33409e736db26ca9f07430af
haikelfazzani <haikel.fazzani@hotmail.com>
2022-02-16 12:12:00 +0100

    1.0.0

--- src/servino.js
+++ src/servino.js
@@ -1,38 +1,10 @@
-const path = require('path').posix;
-const fs = require('fs');
 const Monitor = require('./monitor/Monitor');
-
-let config = {};
+const Config = require('./util/Config')
 
 module.exports = function Servino(options) {
-  let rootPath = path.join(process.cwd(), (options.root || '.'));
 
-  if (options.configFile) {
-    try {
-      const jsonConfig = fs.readFileSync(
-        path.resolve(process.cwd(), options.configFile, 'servino.json'), {
-        encoding: 'utf8'
-      });
+  const config = Config(options);
 
-      config = JSON.parse(jsonConfig);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-  else {
-    config = {
-      host: options.host || '0.0.0.0',
-      port: options.port || 8125,
-      root: rootPath,
-      wdir: options.wdir ? options.wdir.split(',') : [rootPath],
-      delay: options.delay || 100,
-      ignore: options.ignore || /node_modules|(^|[\/\\])\../,
-      inject: options.inject || true,
-      open: options.open || true,
-      verbose: options.verbose || true
-    }
-  }
-
-  // Monitor.emit('start-process', config);
-  // Monitor.emit('start-watching-files', config);
+  Monitor.emit('start-process', config);
+  Monitor.emit('start-watching-files', config);
 }
---
+++ src/util/Config.js
@@ -0,0 +1,36 @@
+const path = require('path').posix;
+const fs = require('fs');
+
+let initConfig = {};
+
+module.exports = function Config(options) {
+  let rootPath = path.join(process.cwd(), (options.root || '.'));
+
+  if (options.configFile) {
+    try {
+      const jsonConfig = fs.readFileSync(
+        path.resolve(process.cwd(), options.configFile, 'servino.json'), {
+        encoding: 'utf8'
+      });
+
+      initConfig = JSON.parse(jsonConfig);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  else {
+    initConfig = {
+      host: options.host || '0.0.0.0',
+      port: options.port || 8125,
+      root: rootPath,
+      wdir: options.wdir ? options.wdir.split(',') : [rootPath],
+      delay: options.delay || 100,
+      ignore: options.ignore || /node_modules|(^|[\/\\])\../,
+      inject: options.inject || true,
+      open: options.open || true,
+      verbose: options.verbose || true
+    }
+  }
+
+  return initConfig
+}
